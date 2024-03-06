cf9e0f2f055e6f9b468b5e4c987685aab847ab0c
iluna007 <146373472+iluna007@users.noreply.github.com>
2024-03-06 15:10:12 +0000

    install jest

---
+++ unitConverter.js
@@ -0,0 +1,14 @@
+function fromDollarToYen(amount) {
+    // Conversion rate: 1 USD = 110 Yen
+    return amount * 110;
+}
+
+function fromEuroToDollar(amount) {
+    // Conversion rate: 1 Euro = 1.17 USD
+    return amount * 1.17;
+}
+
+function fromYenToPound(amount) {
+    // Conversion rate: 1 Yen = 0.0067 GBP
+    return amount * 0.0067;
+}
